e9ecbd3b876b8d13da293036f928fd5517c2a924
Simon Abadi <Simon.george986@gmail.com>
2024-10-15 15:20:23 -0500

    Finished with the structure of the components & Finished with firebase authentication settings

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,1 +1,35 @@
+import { createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, updatePassword } from "firebase/auth";
 import { auth } from "./firebase";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
+
+
+export const doCreateUserWithEmailandPassword = async (email, password) => {
+    return createUserWithEmailAndPassword(auth , email, password);
+};
+
+export const doSignInWithEmailandPassword=(email , password) => {
+    return signInWithEmailAndPassword(auth , email, password);
+};
+
+export const doSignInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth , provider);
+    return result;
+};
+
+export const doSignOut = () => {
+    return auth.signOut();
+};
+
+export const doPasswordReset = (email) =>{
+    return sendPasswordResetEmail(auth, email);
+};
+
+export const doPasswordChange = (password) =>{
+    return updatePassword(auth.currentUser, password);
+};
+export const doSendEmailVerification = () => {
+    return sendEmailVerification(auth.currentUser, {
+      url: `${window.location.origin}/home`,
+    });
+};
